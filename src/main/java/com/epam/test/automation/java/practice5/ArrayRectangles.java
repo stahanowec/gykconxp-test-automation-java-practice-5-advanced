745ea046e316deadc8091ba27fee13fccd95ffd2
API Client <api-client@example.com>
2021-08-16 17:59:48 +0000

    Initial commit

---
+++ src/main/java/com/epam/test/automation/java/practice5/ArrayRectangles.java
@@ -0,0 +1,52 @@
+package com.epam.test.automation.java.practice5;
+
+/**
+ * <summary>
+ * Implement class according to description of task.
+ * </summary>
+ */
+public class ArrayRectangles {
+    //TODO: Delete line below and write your own solution;
+    //TODO: implement constructors according to description of task
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public boolean addRectangle(Rectangle rectangle) {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public int numberMaxArea() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public int numberMinPerimeter() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public int numberSquares() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/main/java/com/epam/test/automation/java/practice5/Rectangle.java
@@ -0,0 +1,80 @@
+package com.epam.test.automation.java.practice5;
+
+public class Rectangle {
+
+    public Rectangle(double a, double b) {
+        //TODO: Implement constructor with 'a' and 'b' parameters;
+        //TODO: set sideA and sideB to specified values;
+    }
+
+    public Rectangle(double a) {
+        //TODO: Implement constructor with 'a' parameter;
+        //TODO: set sideB to default value;
+    }
+
+    public Rectangle() {
+        //TODO: Implement default constructor;
+        //TODO: set sideA and sideB to default values;
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public double getSideA() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public double getSideB() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public double area() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public double perimeter() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public boolean isSquare() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     */
+    public void replaceSides() {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+}
